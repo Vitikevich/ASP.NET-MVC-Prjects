b813d1b050d042dfb77ec222e0a3a7808a3ecf36
Viktor <vitikevich@mail.ru>
2018-06-05 21:51:26 +0700

    Изменён UsersController выброшено немного лишнего. Изменён утилитный класс

---
+++ UseXPagedListMVC/Utils/UsersDefaultItemsPerPage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace UseXPagedListMVC.Utils
+{
+    public class UsersDefaultItemsPerPage
+    {
+        public static int SelectedValue { get => 10; }
+        public static int[] SelectionItems { get => new int[] { 10, 20, 35, 50, 75, 100 }; }
+
+        public static SelectList ItemsPerPageList { get => new SelectList(SelectionItems, selectedValue: SelectedValue); }
+
+        
+
+}
+}
